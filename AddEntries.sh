4d71ed284100ea226c517c56dc7144f09567d51b
raxxy <cam@kingternet.com>
2015-03-12 23:24:18 +1000

    initial

---
+++ AddEntries.sh
@@ -0,0 +1,20 @@
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh/80/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh1/81/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh2/82/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh3/83/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh4/84/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh5/85/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh6/86/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh7/87/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh8/88/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh9/89/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh11/80/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh12/82/K3Y
+curl -i -X POST http://localhost:3000/save/DM-Test/TimEh13/83/K3Y
+
+curl -i -X POST http://localhost:3000/save/DM-icle/TimEh/80/K3Y
+curl -i -X POST http://localhost:3000/save/DM-icle/TimEh1/81/K3Y
+curl -i -X POST http://localhost:3000/save/DM-icle/TimEh2/82/K3Y
+curl -i -X POST http://localhost:3000/save/DM-icle/TimEh3/83/K3Y
+curl -i -X POST http://localhost:3000/save/DM-icle/TimEh4/84/K3Y
+
